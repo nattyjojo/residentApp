42c8deaa0d4b4751006d03567c1c574ef42a12ed
nattyjojo <nattyjojo9@gmail.com>
2023-12-27 14:14:24 +0100

    fixed logOut

--- app/clientsComponents/hamburger.js
+++ app/clientsComponents/hamburger.js
@@ -86,6 +86,7 @@ const HamburgerButton = (children) => {
         {isValid ? (
           <div
             onClick={() => {
+              setIsvalid(!isValid);
               logOut();
             }}
           >
